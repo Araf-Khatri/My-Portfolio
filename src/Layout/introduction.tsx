a552fe291eee6da55b07f231456676d27d222739
Araf-Khatri <arafkhatri0@gmail.com>
2023-05-13 19:13:05 +0530

    tech stack without marquee

--- src/Layout/introduction.tsx
+++ src/Layout/introduction.tsx
@@ -6,46 +6,51 @@ import TechStack from "../Components/tech-stack";
 import SectionBlurBg from "../Components/section-components/blur-bg";
 
 const Introduction: FC = () => {
-  console.log('rerendered')
+  console.log("rerendered");
   return (
     <div className="relative" id="home">
       <SectionBlurBg top={"top-[20rem]"} left={"left-[15%]"} />
 
-      <Section classname={"pt-24 pb-16 relative"}>
-        <div className="flex justify-between gap-8 items-center">
+      <Section classname={"pt-24 pb-16 relative flex flex-col gap-8 "}>
+        <div className="flex lg:justify-between gap-8 items-center">
           <div className="flex flex-col gap-9">
             <div>
-              <p className="text-6xl font-semibold whitespace-nowrap">
+              {/* 3xl:text-6xl lg:text-5xl  */}
+              <p className="lg:text-5.5xl xl:text-6xl  whitespace-nowrap font-semibold ">
                 Hi! I Am Araf Khatri
               </p>
-              <p className="text-right">
+              <p className="2xl:text-right">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
                 provident{" "}
               </p>
             </div>
 
-            <TechStack />
+            {/* <TechStack classname="hidden lg:flex" /> */}
           </div>
-          <div>
-            {/* relative z-10 w-[38rem] h-96 p-3 bg-overflow-hidden  */}
-            <div
-              className={`relative z-10 aspect-video max-w-4xl min-w-[35rem]  p-3 bg-overflow-hidden 
+          {/* <div> */}
+          {/* relative z-10 w-[38rem] h-96 p-3 bg-overflow-hidden  */}
+          <div
+            className={`relative z-10 aspect-video 
+              max-w-[28rem]
+              min-w-[15rem] lg:min-w-[24rem]  p-3 bg-overflow-hidden 
 
             before:content-[''] before:absolute before:top-1 before:left-1 before:h-10 before:w-16 before:bg-secondary-600 before:z-[-1]
 
             after:content-[''] after:absolute after:bottom-1 after:right-1 after:h-10 after:w-16 after:bg-secondary-600 after:z-[-1]
             
             `}
-            >
-              <img
-                src={profilePhoto}
-                className={`object-cover
+          >
+            <img
+              src={profilePhoto}
+              className={`object-cover
             shadow-2xl shadow-secondary-500-25
+            w-full
             `}
-              />
-            </div>
+            />
+            {/* </div> */}
           </div>
         </div>
+        <TechStack hidePara={true} classname="flex " />
       </Section>
     </div>
   );
